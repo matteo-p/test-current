0079582d336b8b88cea205977dd653c40451575d
matteo-p <matteoparisi1980@gmail.com>
2018-06-05 18:07:11 +0200

    recipe
    r

--- course-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ course-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('test recipe', 'this is the twst recipe', 'https://cdn.pixabay.com/photo/2017/12/21/13/25/orange-3031789_960_720.jpg')
+  ];
 
   constructor() { }
 
