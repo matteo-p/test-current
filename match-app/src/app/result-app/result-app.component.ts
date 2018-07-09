48192fc5d30e186cff9b5001f11f3827c3c51f61
matteo-p <matteoparisi1980@gmail.com>
2018-07-09 14:37:08 +0200

    local json for example in comments

--- match-app/src/app/result-app/result-app.component.ts
+++ match-app/src/app/result-app/result-app.component.ts
@@ -31,6 +31,22 @@ export class ResultAppComponent implements OnInit {
     this.update();
   }
 
+ /*  ngOnInit() {
+    fetch('../../assets/scores.json')
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.theDate = (this.d.getDate() + ' / ' + this.d.getMonth() + ' / ' + this.d.getFullYear());
+      this.matchesData = data;
+
+     Object.values(this.matchesData).forEach( (element) => {
+        element.datetime = new Date(element.datetime);
+        element.datetime = element.datetime.getHours();
+      });
+    });
+    //this.update();
+  } */
+
   update() {
     setInterval(() => {
       fetch('https://worldcup.sfg.io/matches/today')
